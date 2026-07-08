a2afd48537f826b1338a8a5ba4ea8ea4ce857ff4
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-08 06:58:12 +0545

    u

--- AI/lab5/lab5.1/lab5-1.py
+++ AI/lab5/lab5.1/lab5-1.py
@@ -1,2 +1,5 @@
-location=input("Which block is the vaccum currently in ?(A, B, C, or D) [or 'exit' to quit]:").string().lower()
 
+print("  2x2 Grid Vacuum Agent Simulation(simple reflex agent) ")
+print("  Grid Layout: [A][B]")
+print("               [C][D]")
+location=input("Which block is the vaccum currently in ?(A, B, C, or D) [or 'exit' to quit]:").string().lower()
