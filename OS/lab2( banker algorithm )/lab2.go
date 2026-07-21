d63c502f21e96a591ecf869111ceb443e1cbff97
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-21 14:04:52 +0545

    initial setup of banker's algorithm with user input

---
+++ OS/lab2( banker algorithm )/lab2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, m int
+
+	fmt.Print("Enter the number of processes: ")
+	fmt.Scan(&n)
+
+	fmt.Print("Enter the number of resource types: ")
+	fmt.Scan(&m)
+}
