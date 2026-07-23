dd2e41d9fda07f42013d68c81407d975e6a365e9
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-23 10:57:11 +0545

    defined aconstructor that is responsible for fulfilling the struct with user inputted values

--- OS/lab5(page replacement)/fifo.go
+++ OS/lab5(page replacement)/fifo.go
@@ -5,3 +5,24 @@ type FIFO struct {
 	frames   []int        //holds the current state of frames
 	pageMap  map[int]bool // fast lookup to check if the process are inside ther frames
 }
+
+func NewFIFO(capacity int) *FIFO {
+	return &FIFO{
+		capacity: capacity,
+		frames:   make([]int, 0, capacity),
+		pageMap:  make(map[int]bool),
+	}
+}
+
+func (f *FIFO) Access(page int) bool {
+	//this is the condition of the page hit //
+	if f.pageMap[page] {
+		return true
+	}
+
+	if len(f.frames) == f.capacity {
+		victim := f.frames[0]
+		f.frames = f.frames[1:]
+		delete(f.pageMap, victim)
+	}
+}
