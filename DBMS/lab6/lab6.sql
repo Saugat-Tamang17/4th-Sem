5d4939aa6dd84933620c3eac835264c45762034e
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-07-28 20:19:38 +0545

    second question for question

--- DBMS/lab6/lab6.sql
+++ DBMS/lab6/lab6.sql
@@ -10,3 +10,27 @@ ON e.officeCode = o.officeCode;
 SELECT * FROM Employee_View;
 
 
+-- 2.Create a view that contains information about customerNumber, customerName, Full contact name, orderNumber, order status and total amount of each order.
+CREATE VIEW Customer_Order_View AS
+SELECT
+    c.customerNumber,
+    c.customerName,
+    CONCAT(c.contactFirstName, ' ', c.contactLastName) AS ContactName,
+    o.orderNumber,
+    o.status,
+    SUM(od.quantityOrdered * od.priceEach) AS TotalAmount
+FROM customers c
+JOIN orders o
+ON c.customerNumber = o.customerNumber
+JOIN orderdetails od
+ON o.orderNumber = od.orderNumber
+GROUP BY
+    c.customerNumber,
+    c.customerName,
+    ContactName,
+    o.orderNumber,
+    o.status;
+SELECT * FROM Customer_Order_View;
+show tables;
+
+
