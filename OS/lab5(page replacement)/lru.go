af52cfeeaead538564886859f898719563703efc
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-05 21:25:14 +0545

    initial setup for the lru algorithm

---
+++ OS/lab5(page replacement)/lru.go
@@ -0,0 +1,11 @@
+package main
+
+func lru(pages []int, capacity int) {
+
+}
+
+func main() {
+	referenceString := []int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3}
+	capacity := 3
+	lru(referenceString, capacity)
+}
