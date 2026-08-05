41211e2ad0cae545d353519d5ddcaa5d9a8e860d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-05 20:39:16 +0545

    initial setup for the fifo page replacement algorithm

--- OS/lab5(page replacement)/fifo.go
+++ OS/lab5(page replacement)/fifo.go
@@ -1,28 +1,8 @@
 package main
 
-type FIFO struct {
-	capacity int
-	frames   []int        //holds the current state of frames
-	pageMap  map[int]bool // fast lookup to check if the process are inside ther frames
-}
+import "fmt"
 
-func NewFIFO(capacity int) *FIFO {
-	return &FIFO{
-		capacity: capacity,
-		frames:   make([]int, 0, capacity),
-		pageMap:  make(map[int]bool),
-	}
-}
+func fifo(pages []int , capacity int) int {
 
-func (f *FIFO) Access(page int) bool {
-	//this is the condition of the page hit //
-	if f.pageMap[page] {
-		return true
-	}
-
-	if len(f.frames) == f.capacity {
-		victim := f.frames[0]
-		f.frames = f.frames[1:]
-		delete(f.pageMap, victim)
-	}
+	
 }
