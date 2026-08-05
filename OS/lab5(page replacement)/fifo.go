fcc18f2d4b36f386d3d2d96bda8442795bc5328d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-05 21:21:03 +0545

    completed te fifo page replacement algorithm

--- OS/lab5(page replacement)/fifo.go
+++ OS/lab5(page replacement)/fifo.go
@@ -10,42 +10,35 @@ func fifo(pages []int, capacity int) int {
 	queue := make([]int, 0, capacity)
 	pagefaults := 0
 
-	//printing the headers ( top part ) //
+	// printing the headers (top part)
 	header := fmt.Sprintf("%-16s |", "Reference String")
 	for i := 1; i <= capacity; i++ {
 		header += fmt.Sprintf(" Frame %d |", i)
 	}
-
 	header += "action taken"
 	fmt.Println(header)
 	fmt.Println(strings.Repeat("-", len(header)+25))
 
-	//checking if the page is alreadyy in memory
-
 	for _, page := range pages {
 		inFrames := false
 		for _, f := range frames {
-			if f == page { //meaning that it is already inside the memory
+			if f == page {
 				inFrames = true
 				break
 			}
 		}
-		var action string
-		if inFrames {
-			action = "Hit"
-		} else {
+
+		action := "Hit"
+		if !inFrames {
 			pagefaults++
-			//if there is still free memory space
 			if len(frames) < capacity {
 				frames = append(frames, page)
 				queue = append(queue, page)
 				action = fmt.Sprintf("Page %d Loaded", page)
 			} else {
-				//evicting the oldest page ( front page in the queue)
 				evicted := queue[0]
 				queue = queue[1:]
 
-				//replacing the evicted pages in the frame slices
 				for idx, f := range frames {
 					if f == evicted {
 						frames[idx] = page
@@ -55,19 +48,28 @@ func fifo(pages []int, capacity int) int {
 				queue = append(queue, page)
 				action = fmt.Sprintf("Evicted %d-> Loaded %d", evicted, page)
 			}
-			//printing the frame contents and the current action
-			row := fmt.Sprintf("%-16d|", page)
-			for i := 0; i < capacity; i++ {
-				if i < len(frames) {
-					row += fmt.Sprintf("%-7d |", frames[i])
-				} else {
-					row += fmt.Sprintf("%-7s |", "-") //empty
-				}
-			}
-			row += "" + action
-			fmt.Println(row)
 		}
+
+		row := fmt.Sprintf("%-16d|", page)
+		for i := 0; i < capacity; i++ {
+			if i < len(frames) {
+				row += fmt.Sprintf("%-7d |", frames[i])
+			} else {
+				row += fmt.Sprintf("%-7s |", "-")
+			}
+		}
+		row += action
+		fmt.Println(row)
 		fmt.Println(strings.Repeat("-", len(header)+25))
-		fmt.Printf("Total Page Faults: %d\n", pageFaults)
+		fmt.Printf("Total Page Faults: %d\n", pagefaults)
 	}
+
+	return pagefaults
+}
+
+func main() {
+	referenceString := []int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3}
+	capacity := 3
+
+	fifo(referenceString, capacity)
 }
