e87f267b93351cfb6946f9eef21a632584d9f33e
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-05 20:47:03 +0545

    printing the header  of the table

--- OS/lab5(page replacement)/fifo.go
+++ OS/lab5(page replacement)/fifo.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func fifo(pages []int, capacity int) int {
 	frames := make([]int, 0, capacity)
 	queue := make([]int, 0, capacity)
 	pagefaults := 0
 
+	//printing the headers ( top part ) //
+	header := fmt.Sprintf("%-16s |", "Reference String")
+	for i := 1; i <= capacity; i++ {
+		header += fmt.Sprintf(" Frame %d |", i)
+	}
 }
