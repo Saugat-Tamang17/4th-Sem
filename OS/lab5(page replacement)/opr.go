0b0b747dc8969265708f9cb024ed5166e47b8cfd
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-07 22:36:36 +0545

    polished version of the algorithm

--- OS/lab5(page replacement)/opr.go
+++ OS/lab5(page replacement)/opr.go
@@ -1,7 +1,15 @@
 package main
-func opr(pages[]int,capacity[]){
+
+import (
+	"fmt"
+	"strings"
+)
+
+func opr(pages []int, capacity int) {
 	frames := make([]int, 0, capacity)
 	pageFaults := 0
+
+	// Print Header
 	header := fmt.Sprintf("%-16s |", "Reference String")
 	for i := 1; i <= capacity; i++ {
 		header += fmt.Sprintf(" Frame %d |", i)
@@ -9,6 +17,8 @@ func opr(pages[]int,capacity[]){
 	header += " Action Taken"
 	fmt.Println(header)
 	fmt.Println(strings.Repeat("-", len(header)+25))
+
+	// Process each page
 	for i, page := range pages {
 		inFrames := false
 		for _, f := range frames {
@@ -16,7 +26,9 @@ func opr(pages[]int,capacity[]){
 				inFrames = true
 				break
 			}
-		}var action string
+		}
+
+		var action string
 
 		if inFrames {
 			action = "Hit"
@@ -24,34 +36,62 @@ func opr(pages[]int,capacity[]){
 			pageFaults++
 			if len(frames) < capacity {
 				frames = append(frames, page)
-				action = fmt.Sprintf("Page %d Loaded", page)}else {
+				action = fmt.Sprintf("Page %d Loaded", page)
+			} else {
 				// Find the frame page that will NOT be used for the longest time in the future
 				replaceIdx := -1
 				farthestUse := -1
 
 				for idx, f := range frames {
 					nextUse := -1
+
+					// Look ahead in the remaining reference string
 					for j := i + 1; j < len(pages); j++ {
 						if pages[j] == f {
 							nextUse = j
 							break
 						}
 					}
+
+					// If a page is never referenced again, it is the best candidate to evict immediately
 					if nextUse == -1 {
 						replaceIdx = idx
 						break
 					}
+
+					// Otherwise, select the page whose next reference is farthest in the future
 					if nextUse > farthestUse {
 						farthestUse = nextUse
 						replaceIdx = idx
 					}
 				}
-				}
 
+				evicted := frames[replaceIdx]
+				frames[replaceIdx] = page
+				action = fmt.Sprintf("Evicted %d -> Loaded %d", evicted, page)
+			}
+		}
 
-//opr stands for optimal page replacement//
-func main(){
+		// Print frame contents and current action
+		row := fmt.Sprintf("%-16d |", page)
+		for k := 0; k < capacity; k++ {
+			if k < len(frames) {
+				row += fmt.Sprintf(" %-7d |", frames[k])
+			} else {
+				row += fmt.Sprintf(" %-7s |", "-") // Empty frame slot
+			}
+		}
+		row += " " + action
+		fmt.Println(row)
+	}
+
+	fmt.Println(strings.Repeat("-", len(header)+25))
+	fmt.Printf("Total Page Faults: %d\n", pageFaults)
+}
+
+func main() {
 	referenceString := []int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3}
-	capacity :=3
-	opr(referenceString,capacity)
+	capacity := 3
+
+	opr(referenceString, capacity)
 }
