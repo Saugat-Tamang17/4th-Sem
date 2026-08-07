6d6af1229ef16d7de8020bded7147a1bd73d923f
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-07 22:19:21 +0545

    frames slice and ol classic pageFault declarations

--- OS/lab5(page replacement)/opr.go
+++ OS/lab5(page replacement)/opr.go
@@ -1,8 +1,11 @@
 package main
 func opr(pages[]int,capacity[]){
-
+	frames := make([]int, 0, capacity)
+	pageFaults := 0
 }
 
+
+//opr stands for optimal page replacement//
 func main(){
 	referenceString := []int{7, 0, 1, 2, 0, 3, 0, 4, 2, 3}
 	capacity :=3
