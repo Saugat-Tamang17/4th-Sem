cb9009ae2dd265d4b22bbf54e3b7db4eb41920de
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-07 21:23:06 +0545

    fixed the mistake on that for loop part

--- OS/lab5(page replacement)/lru.go
+++ OS/lab5(page replacement)/lru.go
@@ -1,14 +1,17 @@
 package main
 
+import "fmt"
+
 func lru(pages []int, capacity int) {
 	frames := make([]int, 0, capacity)
 	LastUsed := make(map[int]int)
 	pageFaults := 0
 
-	header:=fmt.Sprintf("%-16s |","Reference String")
-	for i:=0,i<capacity;i++{
-		header+=fmt.Sprintf("Frame %d |",i)
+	header := fmt.Sprintf("%-16s |", "Reference String")
+	for i := 0; i <= capacity; i++ {
+		header += fmt.Sprintf("Frame %d |", i)
 	}
+	header += "action taken"
 }
 
 func main() {
