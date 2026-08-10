9ca027e4db3eb9f04e4834af729237f06ae57ac6
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-08-10 07:15:26 +0545

    initial setup for the fcfs disk scheduling algorithm

---
+++ OS/lab6_DiskReplacement/fcfs.go
@@ -0,0 +1,11 @@
+package main
+
+func FCFSdisk(queue []int, head int) {
+
+}
+
+func main() {
+	queue := []int{176, 79, 34, 60, 92, 11, 41, 114}
+	initialHead := 50
+	FCFSdisk(queue, initialHead)
+}
